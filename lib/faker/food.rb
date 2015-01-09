1a926ddd84c6a70e6a30efed2e6afbb5510471a2
Sasha Grodzins <sasha.grodzins@devmynd.com>
2015-01-09 17:15:15 -0600

    faker food stuff

--- lib/faker/food.rb
+++ lib/faker/food.rb
@@ -23,6 +23,18 @@ module Faker
         (fetch('food.description.adjective') + ', ' + fetch('food.description.preparation') + ' ' + fetch('food.animal') + " in a #{self.sauce}").titleize
       end
 
+      def phrases
+       [ "If we #{verb} the #{noun}, we can get to the #{abbreviation} #{noun} through the #{adjective} #{abbreviation} #{noun}!",
+         "We need to #{verb} the #{adjective} #{abbreviation} #{noun}!",
+         "Try to #{verb} the #{abbreviation} #{noun}, maybe it will #{verb} the #{adjective} #{noun}!",
+         "You can't #{verb} the #{noun} without #{ingverb} the #{adjective} #{abbreviation} #{noun}!",
+         "Use the #{adjective} #{abbreviation} #{noun}, then you can #{verb} the #{adjective} #{noun}!",
+         "The #{abbreviation} #{noun} is down, #{verb} the #{adjective} #{noun} so we can #{verb} the #{abbreviation} #{noun}!",
+         "#{ingverb} the #{noun} won't do anything, we need to #{verb} the #{adjective} #{abbreviation} #{noun}!",
+         "I'll #{verb} the #{adjective} #{abbreviation} #{noun}, that should #{noun} the #{abbreviation} #{noun}!"
+         ]
+      end
+
       def price_thing
         random = Random.new
         "$" + ((random.rand(0..100.0) * 100).floor / 100.0).to_s  + ' ' + fetch('food.description.price')
--- test/test_faker_food.rb
+++ test/test_faker_food.rb
@@ -3,9 +3,12 @@ require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 class TestFakerFood < Test::Unit::TestCase
   def setup
     @tester = Faker::Food
+    @animal_list = I18n.translate('faker.food.animal')
   end
 
-  # def test_animal
-  #   assert @
-  # end
+  def test_animal
+    animal_to_eat = @tester.animal
+    assert @tester.animal.match(/\w+/)
+    assert @animal_list.include?(animal_to_eat)
+  end
 end
